d45858f625273b04b865d301c8db8a1a683bf5c5
Diego Malatesta <diego.malatesta@pm.me>
2024-03-16 20:00:17 +0000

    Setup camera

---
+++ src/camera.rs
@@ -0,0 +1,16 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct MainCamera;
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn((Camera2dBundle { ..default() }, MainCamera));
+}
+
+pub struct MainCameraPlugin;
+
+impl Plugin for MainCameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup_camera);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
+mod camera;
+mod paddle;
+
 use bevy::prelude::*;
+use camera::MainCameraPlugin;
+use paddle::Paddle;
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Paddle);
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins((DefaultPlugins, MainCameraPlugin))
+        .add_systems(Startup, setup)
         .add_systems(Update, bevy::window::close_on_esc)
         .run();
 }
